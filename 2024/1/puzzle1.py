7423f47dce655230865e9a1375e1a1bcafde2c2b
Maxime Chambre <enzu.mc@gmail.com>
2024-12-01 11:54:09 +0100

    Add Advent of Code 2024 - 1

---
+++ 2024/1/puzzle1.py
@@ -0,0 +1,42 @@
+# Custom input
+input_data = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+input_data = input_data.split('\n')
+
+
+# Input from file
+input_data = open("input.txt").read().split('\n')
+input_data.pop()
+
+
+# Formatting
+left_list = []
+right_list = []
+
+for line in input_data:
+    left, right = line.split("   ")
+    left_list.append(int(left))
+    right_list.append(int(right))
+
+left_list.sort()
+right_list.sort()
+
+# Functions
+def getMinDifferences(left_list, right_list):
+    distance = 0
+
+    while len(left_list) > 0 and len(right_list) > 0:
+        left_min = left_list.pop(0)
+        right_min = right_list.pop(0)
+
+        distance += abs(left_min - right_min)
+    
+    return distance
+
+# Code
+
+print(getMinDifferences(left_list, right_list))
---
+++ 2024/1/puzzle2.py
@@ -0,0 +1,48 @@
+# Custom input
+input_data = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+input_data = input_data.split('\n')
+
+
+# Input from file
+input_data = open("input.txt").read().split('\n')
+input_data.pop()
+
+
+# Formatting
+left_list = []
+right_list = []
+
+for line in input_data:
+    left, right = line.split("   ")
+    left_list.append(int(left))
+    right_list.append(int(right))
+
+
+# Functions
+def getSimilarityScore(left_list, right_list):
+    score = 0
+
+    right_occurences = {}
+
+    # right list numbers occurences
+    for num in right_list:
+        if num not in right_occurences:
+            right_occurences[num] = 1
+        else:
+            right_occurences[num] += 1
+    
+    # score
+    for num in left_list:
+        if num in right_occurences:
+            score += num * right_occurences[num]
+    
+    return score
+
+# Code
+
+print(getSimilarityScore(left_list, right_list))
