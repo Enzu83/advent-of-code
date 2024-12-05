12732faccb8c9b38cbb195b8fa57f2042040cc04
Maxime Chambre <enzu.mc@gmail.com>
2024-12-05 23:07:06 +0100

    add: Advent of Code 2024 Day 5

---
+++ 2024/05/puzzle2.py
@@ -0,0 +1,102 @@
+# Custom input
+input_data = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
+input_data = input_data.split('\n')
+
+
+# Input from file
+input_data = open("input.txt").read().split('\n')
+input_data.pop()
+
+
+# Formatting
+
+rules = {}
+updates = []
+
+for line in input_data:
+    if '|' in line:
+        first, second = line.split('|')
+
+        if first not in rules:
+            rules[first] = [second]
+        else:
+            rules[first].append(second)
+        
+        if second not in rules:
+            rules[second] = []
+    
+    elif ',' in line:
+        updates.append(line.split(','))
+
+
+# Functions
+
+def getRightOrder(update, rules):
+    order = []
+
+    for num in update:
+        inserted = False
+
+        for i in range(len(order)):
+            if order[i] in rules[num]:
+                order = order[:i] + [num] + order[i:]
+                inserted = True
+                break
+        
+        if inserted is False:
+            order.append(num)
+
+    return order
+
+
+def checkRightOrder(update, rules):
+    for i, num in enumerate(update):
+        for next_num in rules[num]:
+            for j in range(i):
+                if next_num == update[j]:
+                    return False
+
+    return True
+
+def countRightUpdates(updates, rules):
+    result = 0
+
+    for update in updates:
+        if checkRightOrder(update, rules) is False:
+            update = getRightOrder(update, rules)
+
+            result += int(update[len(update)//2])
+
+    return result
+
+
+# Code
+
+print(countRightUpdates(updates, rules))
