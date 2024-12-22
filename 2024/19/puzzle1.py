644f102c6f9d3efbf0c973e6e37c3f3bed5f6feb
Maxime <enzu.mc@gmail.com>
2024-12-22 14:05:00 +0100

    add: Advent of Code 2024 Day 19 Part 1

--- 2024/19/puzzle1.py
+++ 2024/19/puzzle1.py
@@ -20,73 +20,37 @@ input_data.pop()
 # Formatting
 
 patterns = set()
-designs = set()
+towels = set()
 
 for line in input_data:
     if ',' in line:
-        patterns |= line.split(', ')
+        patterns = set(line.split(', '))
 
     elif len(line) > 0:
-        designs.add(line)
+        towels.add(line)
 
 
 # Functions
 
-"""def addPatterns(patterns, parts, new_part):
-    for part in parts:
-        patterns.add(part + new_part)
+def isDesignPossible(towel, patterns):
+    designs = {pattern for pattern in patterns if towel.startswith(pattern)}
 
-def designPossible(design, patterns):
-    idx = 0
-    part_length = len(design)
-
-    design_parts = []
-
-    # iterate over all colors of the design
-    while idx < len(design):
-        print(len(patterns))
-        # check if the current part of the design is a pattern
-        # if not, increase the size of the pattern to search for new ones
-        while part_length > 0 and design[idx:idx + part_length] not in patterns:
-            part_length -= 1
-
-        # if a matching pattern has been found, add it to the list of the parts
-        if design[idx:idx + part_length] in patterns:
-            addPatterns(patterns, design_parts, design[idx:idx + part_length]) # update the possible patterns
-
-            design_parts.append(design[idx:idx + part_length])
-            idx += part_length
-            part_length = len(design) - idx
-
-        # else, search for smaller previous part
-        elif len(design_parts) > 0:
-            previous_part = design_parts.pop()
-
-            idx -= len(previous_part)
-            part_length = len(previous_part) - 1
-    
-        # if there's no previous part, the design is not possible
-        else:
-            return False
-    
-    return sum(len(part) for part in design_parts) == len(design) """
-
-def designPossible(desired_design, patterns):
-    current_designs = {pattern for pattern in patterns if desired_design.startswith(pattern)}
-
-    while len(current_designs) > 0 and desired_design not in current_designs:
+    while len(designs) > 0 and towel not in designs:
         new_designs = set()
 
-        for design in current_designs:
-            
+        for design in designs:
+            for pattern in patterns:
+                if towel.startswith(design + pattern):
+                    new_designs.add(design + pattern)
 
+        designs = new_designs
 
-def countPossibleDesigns(designs, patterns):
-    return sum(1 for design in designs if designPossible(design, patterns))
+    return towel in designs
+
+def countPossibleDesigns(towels, patterns):
+    return sum(isDesignPossible(towel, patterns) for towel in towels)
 
 
 # Code
 
-print(patterns)
-#print(designPossible("gwruuugwgbbgbwrwgubbbuguwwgrruubwbrgugbgwwbwbbbrgwbubr", patterns))
-print(countPossibleDesigns(designs, patterns))
+print(countPossibleDesigns(towels, patterns))
