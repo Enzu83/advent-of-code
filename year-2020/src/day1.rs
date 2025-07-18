a9b3f47a2721c4010fb478cb346173ece085b7bc
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-07-19 00:05:36 +0200

    feat: add day 1 puzzle 2 solution and clean puzzle 1 solution

--- year-2020/src/day1.rs
+++ year-2020/src/day1.rs
@@ -1,6 +1,7 @@
 use std::{fs::File, io::Read};
 
 use std::cmp::Ordering;
+use std::option::Option::{Some, None};
 
 pub fn run_puzzle(puzzle: u8, input: File) {
     match puzzle {
@@ -18,6 +19,29 @@ fn read_input(input: &mut File) -> String {
     }
 }
 
+fn find_sum_combination(expenses: &[i32], sum: i32) -> Option<(usize, usize)> {
+    let mut left_idx = 0;
+    let mut right_idx = expenses.len() - 1;
+
+    // move left or right pointers depending on their sum
+    // since left value is always less than right value, we know which pointer to move for each case
+    while left_idx < right_idx {
+        match (expenses[left_idx] + expenses[right_idx]).cmp(&sum) {
+            Ordering::Less => left_idx += 1,
+            Ordering::Greater => right_idx -= 1,
+            Ordering::Equal => break,
+        }
+    }
+
+    if left_idx == right_idx {
+        // no solution found
+        None
+    } else {
+        // solution found
+        Some((left_idx, right_idx))
+    }   
+}
+
 fn puzzle_1(mut input: File) {
     let mut expenses: Vec<i32> = read_input(&mut input).lines()
         .map(|s| s.parse::<i32>().unwrap())
@@ -25,25 +49,42 @@ fn puzzle_1(mut input: File) {
 
     expenses.sort();
 
-    let mut left_idx = 0;
-    let mut right_idx = expenses.len() - 1;
+    let (left_idx, right_idx) = find_sum_combination(&expenses, 2020).unwrap();
 
-    while left_idx < right_idx {
-        match (expenses[left_idx] + expenses[right_idx]).cmp(&2020) {
-            Ordering::Less => left_idx += 1,
-            Ordering::Greater => right_idx -= 1,
-            Ordering::Equal => break,
-        }
-    }
-    
     println!("Found matching combination: ({}, {}). Product: {}", 
         expenses[left_idx], 
         expenses[right_idx], 
         expenses[left_idx] * expenses[right_idx]
     );
-
 }
 
 fn puzzle_2(mut input: File) {
-    println!("{}", read_input(&mut input));
+    let mut expenses: Vec<i32> = read_input(&mut input).lines()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    expenses.sort();
+
+    // we will progressively decrease this pointer until we find a combination
+    // which sum is 2020 - expenses[right_idx]
+    let mut right_idx = expenses.len() - 1;
+
+    while right_idx > 0 {
+        match find_sum_combination(&expenses[0..right_idx], 2020 - expenses[right_idx]) {
+            // try again with the previous expense
+            None => right_idx -= 1,
+
+            // solution found
+            Some((left_idx, middle_idx)) => {
+                println!("Found matching combination: ({}, {}, {}). Product: {}", 
+                    expenses[left_idx], 
+                    expenses[middle_idx], 
+                    expenses[right_idx], 
+                    expenses[left_idx] * expenses[middle_idx] * expenses[right_idx]
+                );
+
+                break
+            },
+        }
+    }
 }
