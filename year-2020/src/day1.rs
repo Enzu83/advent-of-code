1e320bd89ed885e04aec5f39895e849d811cd97b
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-07-18 23:02:05 +0200

    feat: add day 1 puzzle 1 solution

--- year-2020/src/day1.rs
+++ year-2020/src/day1.rs
@@ -1,5 +1,7 @@
 use std::{fs::File, io::Read};
 
+use std::cmp::Ordering;
+
 pub fn run_puzzle(puzzle: u8, input: File) {
     match puzzle {
         1 => puzzle_1(input),
@@ -17,7 +19,29 @@ fn read_input(input: &mut File) -> String {
 }
 
 fn puzzle_1(mut input: File) {
-    println!("{}", read_input(&mut input));
+    let mut expenses: Vec<i32> = read_input(&mut input).lines()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    expenses.sort();
+
+    let mut left_idx = 0;
+    let mut right_idx = expenses.len() - 1;
+
+    while left_idx < right_idx {
+        match (expenses[left_idx] + expenses[right_idx]).cmp(&2020) {
+            Ordering::Less => left_idx += 1,
+            Ordering::Greater => right_idx -= 1,
+            Ordering::Equal => break,
+        }
+    }
+    
+    println!("Found matching combination: ({}, {}). Product: {}", 
+        expenses[left_idx], 
+        expenses[right_idx], 
+        expenses[left_idx] * expenses[right_idx]
+    );
+
 }
 
 fn puzzle_2(mut input: File) {
