f7638c07832c0542390f2793cd80f10ed2d04629
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-07-19 00:55:05 +0200

    feat: add day 2 puzzle 1 solution

--- year-2020/src/day2.rs
+++ year-2020/src/day2.rs
@@ -16,8 +16,68 @@ fn read_input(input: &mut File) -> String {
     }
 }
 
+#[derive(Debug)]
+struct Policy {
+    letter: char,
+    min: u32,
+    max: u32,
+}
+
+fn parse_policy_and_password(line: &str) -> (Policy, String) {
+    let mut splited_line = line.split_ascii_whitespace();
+
+    // range of the policy
+    let range: Vec<u32> = splited_line.next().unwrap()
+        .split('-')
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+
+    let (min, max) = (range[0], range[1]);
+
+    // letter used for the policy
+    let letter = splited_line.next().unwrap()
+        .chars().next().unwrap();
+
+    // password to test
+    let password = splited_line.next().unwrap().to_string();
+
+    let policy = Policy {
+        letter,
+        min,
+        max,
+    };
+
+    (policy, password)
+}
+
+fn count_occurrences_in_string(string: &String, occurrence: char) -> u32 {
+    let mut count_occurrences = 0;
+    
+    for c in string.chars() {
+        if c == occurrence {
+            count_occurrences += 1;
+        }
+    }
+
+    count_occurrences
+}
+
 fn puzzle_1(mut input: File) {
-    println!("{}", read_input(&mut input));
+    let password_list: Vec<(Policy, String)> = read_input(&mut input).lines()
+        .map(|line| parse_policy_and_password(line))
+        .collect();
+
+    let mut correct_passwords = 0;
+
+    for (policy, password) in password_list {
+        let occurrences = count_occurrences_in_string(&password, policy.letter);
+
+        if policy.min <= occurrences && occurrences <= policy.max {
+            correct_passwords += 1
+        }
+    }
+
+    println!("Correct passwords: {}", correct_passwords);
 }
 
 fn puzzle_2(mut input: File) {
