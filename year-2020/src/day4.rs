2ad9235b5f69a45e4341aa3bb9e0e073f0351792
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-07-19 13:33:06 +0200

    feat: day 4 solutions

---
+++ year-2020/src/day4.rs
@@ -0,0 +1,187 @@
+use std::{fs::File, io::Read};
+
+use std::option::Option::{Some, None};
+
+pub fn run_puzzle(puzzle: u8, input: File) {
+    match puzzle {
+        1 => puzzle_1(input),
+        2 => puzzle_2(input),
+        other => panic!("Unknown puzzle number: {}", other),
+    }
+}
+
+fn read_input(input: &mut File) -> String {
+    let mut content = String::new();
+    match input.read_to_string(&mut content) {
+        Ok(_) => return content,
+        Err(e) => panic!("Couldn't read the input: {}", e),
+    }
+}
+
+#[derive(Debug)]
+struct Passport {
+    byr: Option<String>, // Birth Year
+    iyr: Option<String>, // Issue Year
+    eyr: Option<String>, // Expiration Year
+    hgt: Option<String>, // Height
+    hcl: Option<String>, // Hair Color
+    ecl: Option<String>, // Eye Color
+    pid: Option<String>, // Passport ID
+    cid: Option<String>, // Country ID
+}
+
+impl Passport {
+    pub fn new() -> Self {
+        Passport { byr: None, iyr: None, eyr: None, hgt: None, hcl: None, ecl: None, pid: None, cid: None }
+    }
+
+    pub fn has_required_fields(&self) -> bool {
+        [
+            &self.byr,
+            &self.iyr,
+            &self.eyr,
+            &self.hgt,
+            &self.hcl,
+            &self.ecl,
+            &self.pid,
+        ].iter().all(|key| key.is_some())
+    }
+
+    pub fn is_valid(&self) -> bool {
+        if !self.has_required_fields() {
+            return false
+        }
+        
+        // birth year: 1920 - 2002
+        if let Some(birth_year) = self.byr.as_ref().and_then(|s| s.parse::<u32>().ok()) {
+            if birth_year < 1920 || birth_year > 2002 {
+                return false
+            }
+        }
+
+        // issue year: 2010 - 2020
+        if let Some(issue_year) = self.iyr.as_ref().and_then(|s| s.parse::<u32>().ok()) {
+            if issue_year < 2010 || issue_year > 2020 {
+                return false
+            }
+        }
+
+        // expiration year: 2020 - 2030
+        if let Some(expiration_year) = self.eyr.as_ref().and_then(|s| s.parse::<u32>().ok()) {
+            if expiration_year < 2020 || expiration_year > 2030 {
+                return false
+            }
+        }
+
+        // height: 50-193 cm, 9-76 in
+        if let Some((height, unit)) = self.hgt.as_ref().and_then(|s| Some(s.split_at(s.len()-2))) {
+            let height = height.parse::<u32>().unwrap();
+
+            match unit {
+                "cm" => if height < 150 || height > 193 {
+                    return false
+                },
+                "in" => if height < 9 || height > 76 {
+                    return false
+                },
+                _ => return false,
+            }
+        }
+
+        // hair color: #[hex value]
+        let mut hair_color = self.hcl.as_ref().unwrap().chars();
+        
+        // check '#' char before hex value
+        if hair_color.next().unwrap() != '#' {
+            return false
+        }
+
+        // check hex value
+        let hex_value: Vec<char> = hair_color.collect();
+
+        if hex_value.len() != 6 {
+            return false
+        }
+
+        for digit in hex_value {
+            if !digit.is_ascii_hexdigit() {
+                return false
+            }
+        }
+
+        // eye color: [amb, blu, brn, gry, grn, hzl, oth]
+        let eye_color = self.ecl.as_ref().unwrap().as_str();
+        if !["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&eye_color) {
+            return false
+        }
+
+        // passport id: 9 digits long
+        let passport_id = self.pid.as_ref().unwrap();
+
+        if !(passport_id.len() == 9 && passport_id.parse::<u32>().is_ok()) {
+            return false
+        }
+
+        return true
+    }
+}
+
+fn get_passport_from_raw(raw_passport: &str) -> Passport {
+    let mut passport = Passport::new();
+
+    for field in raw_passport.split_whitespace() {
+        if let Some((key, value)) = field.split_once(':') {
+             match key {
+                "byr" => passport.byr = Some(value.to_string()),
+                "iyr" => passport.iyr = Some(value.to_string()),
+                "eyr" => passport.eyr = Some(value.to_string()),
+                "hgt" => passport.hgt = Some(value.to_string()),
+                "hcl" => passport.hcl = Some(value.to_string()),
+                "ecl" => passport.ecl = Some(value.to_string()),
+                "pid" => passport.pid = Some(value.to_string()),
+                "cid" => passport.cid = Some(value.to_string()),
+                other => println!("Unknown Passport key found: {}", other),
+             }
+        }
+    }
+
+    passport
+}
+
+fn get_passports_from_input(input: &mut File) -> Vec<Passport> {
+    let mut passports = Vec::new();
+
+    for raw_password in read_input(input).split("\n\n") {
+        passports.push(get_passport_from_raw(raw_password));
+    }
+
+    passports
+}
+
+fn puzzle_1(mut input: File) {
+    let passports = get_passports_from_input(&mut input);
+
+    let mut valid_passports = 0;
+
+    for passport in passports {
+        if passport.has_required_fields() {
+            valid_passports += 1;
+        }
+    }
+
+    println!("Valid passports: {}", valid_passports);
+}
+
+fn puzzle_2(mut input: File) {
+    let passports = get_passports_from_input(&mut input);
+
+    let mut valid_passports = 0;
+
+    for passport in passports {
+        if passport.is_valid() {
+            valid_passports += 1;
+        }
+    }
+
+    println!("Valid passports: {}", valid_passports);
+}
