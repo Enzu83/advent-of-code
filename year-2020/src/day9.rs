bd33853aaa54d581f49da86f95728ed08b7a3805
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-09-16 01:59:53 +0200

    feat: 2020 day 9 solution sketch

---
+++ year-2020/src/day9.rs
@@ -0,0 +1,36 @@
+use std::{fs::File, io::Read};
+
+pub fn run_puzzle(puzzle: u8, input: File) {
+    match puzzle {
+        1 => puzzle_1(input),
+        2 => puzzle_2(input),
+        other => panic!("Unknown puzzle number: {}", other),
+    }
+}
+
+fn read_input(input: &mut File) -> String {
+    let mut content = String::new();
+    match input.read_to_string(&mut content) {
+        Ok(_) => return content,
+        Err(e) => panic!("Couldn't read the input: {}", e),
+    }
+}
+
+fn find_first_non_valid(numbers: &[u32]) {
+
+}
+
+fn is_next_valid(sums: &Vec<[u32; 25]>) -> bool {
+    false
+}
+
+fn puzzle_1(mut input: File) {
+    let numbers: Vec<u32> = read_input(&mut input)
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect();
+}
+
+fn puzzle_2(mut input: File) {
+    println!("{}", read_input(&mut input));
+}
