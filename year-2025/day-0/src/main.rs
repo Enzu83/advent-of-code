a8f88479943e5d7f893a929cf9f649081d140a93
Enzu83 <enzu.mc@gmail.com>
2025-12-06 20:31:59 +0100

    clean shared code and day 1

--- year-2025/day-0/src/main.rs
+++ year-2025/day-0/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input_path = env::args().nth(1).unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let input_path = match env::args().nth(1) {
+        Some(path) => path,
+        None => panic!("Please specify a path for the input file."),
+    };
     let mut file = File::open(input_path)?;
     let mut input = String::new();
     file.read_to_string(&mut input)?;
--- year-2025/day-1/src/main.rs
+++ year-2025/day-1/src/main.rs
@@ -1,76 +1,202 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input_path = env::args().nth(1).unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let input_path = match env::args().nth(1) {
+        Some(path) => path,
+        None => panic!("Please specify a path for the input file.")
+    };    
     let mut file = File::open(input_path)?;
     let mut input = String::new();
     file.read_to_string(&mut input)?;
 
-    count_zeros(input.trim().split("\n").collect());
+    let instructions: Vec<&str> = input.trim().split("\n").collect();
+    let mut dial = Dial::new();
+    dial.init();
+    dial.execute_instructions(&instructions)?;
+
+    println!("Pointed at zero: {}", dial.pointed_zero);
+    println!("Passed zero: {}", dial.passed_zero);
 
     Ok(())
 }
 
-
-enum Direction {
-    LEFT,
-    RIGHT,
+struct Dial {
+    position: i32,
+    pointed_zero: i32,
+    passed_zero: i32,
 }
 
-fn decode_rotation(rotation: &str) -> (Direction, i32) {
-    let (direction_letter, amount) = rotation.split_at(1);
-
-    match direction_letter {
-        "L" => (Direction::LEFT, amount.parse::<i32>().unwrap()),
-        "R" => (Direction::RIGHT, amount.parse::<i32>().unwrap()),
-        _ => panic!("unknown direction: {}", direction_letter),
+impl Dial {
+    pub fn new() -> Self {
+        Self{
+            position: 0,
+            pointed_zero: 0,
+            passed_zero: 0,
+        }
     }
-}
 
-fn move_dial(current_position: i32, rotation: &str) -> (i32, i32) {
-    let mut number_passed_zero = 0;
-    let (direction, amount) = decode_rotation(rotation);
-
-    match direction {
-        Direction::LEFT => {
-            let new_position = current_position - amount;
-            if new_position <= 0 {
-                number_passed_zero = (-new_position) / 100;
-            }
-
-            (new_position % 100, number_passed_zero)
-        },
-        Direction::RIGHT => {
-            let new_position = current_position + amount;
-            if new_position >= 100 {
-                number_passed_zero = new_position / 100;
-            }
-
-            (new_position % 100, number_passed_zero)
-        },
+    pub fn init(&mut self) {
+        self.position = 50;
+        self.pointed_zero = 0;
+        self.passed_zero = 0;
     }
-}
 
-fn count_zeros(rotations: Vec<&str>) {
-    let mut dial_position = 50;
+    fn decode_instruction(&self, instruction: &str) -> Result<i32, Box<dyn Error>> {
+        let (direction, value_str) = instruction.split_at(1);
+        let value = value_str.parse::<i32>()?;
 
-    let mut pointed_at_zero = 0;
+        match direction {
+            "L" => Ok(-value),
+            "R" => Ok(value),
+            other => Err(format!("Unknown rotation direction: {other}.").into())
+        }
+    }
 
-    let mut number_passed_zero;
-    let mut passed_zero = 0;
+    pub fn rotate(&mut self, instruction: &str) -> Result<(), Box<dyn Error>>{
+        let mut rotation = self.decode_instruction(instruction)?;
 
-    for rotation in rotations {
-        (dial_position, number_passed_zero) = move_dial(dial_position, rotation);
+        if rotation.abs() >= 100 {
+            println!("{}, {}, {}", rotation, rotation % 100, (rotation / 100).abs());
+        }
+        // handle 360 rotation
+        if rotation.abs() >= 100 {
+            let full_rotations = (rotation / 100).abs();
+            self.passed_zero += full_rotations;
 
-        if dial_position == 0 {
-            pointed_at_zero += 1;
+            rotation %= 100; // remaining rotation that will change the final position of the dial
         }
 
-        passed_zero += number_passed_zero;
+        self.position += rotation; // apply rotation
+
+        // check if the dial passed 0
+        if self.position <= 0 || self.position >= 100 {
+            self.passed_zero += 1;
+        }
+
+        self.position = self.position.rem_euclid(100); // keep the position in the [0, 99]
+
+        // check if the dial stopped at 0
+        if self.position == 0 {
+            self.pointed_zero += 1;
+        }
+
+        Ok(())
     }
 
-    println!("Pointed at zero: {pointed_at_zero}");
-    println!("Passed zero: {passed_zero}");
+    pub fn execute_instructions(&mut self, instructions: &Vec<&str>) -> Result<(), Box<dyn Error>> {
+        for instruction in instructions {
+            self.rotate(instruction)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn position_after_rotation() -> Result<(), Box<dyn Error>> {
+        let mut dial = Dial::new();
+
+        dial.rotate("R45")?;
+        assert_eq!(dial.position, 95);
+
+        dial.rotate("R45")?;
+        assert_eq!(dial.position, 40);
+
+        dial.rotate("L20")?;
+        assert_eq!(dial.position, 20);
+
+        dial.rotate("L45")?;
+        assert_eq!(dial.position, 75);
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn below_zero() -> Result<(), Box<dyn Error>> {
+        let mut dial = Dial::new();
+        dial.rotate("L65")?;
+
+        assert_eq!(dial.passed_zero, 1);
+        assert_eq!(dial.pointed_zero, 0);
+        Ok(())
+    }
+
+    #[test]
+    pub fn above_zero() -> Result<(), Box<dyn Error>> {
+        let mut dial = Dial::new();
+        dial.rotate("R65")?;
+
+        assert_eq!(dial.passed_zero, 1);
+        assert_eq!(dial.pointed_zero, 0);
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn at_zero() -> Result<(), Box<dyn Error>> {
+        let mut dial = Dial::new();
+        dial.rotate("R50")?;
+
+        assert_eq!(dial.passed_zero, 1);
+        assert_eq!(dial.pointed_zero, 1);
+
+        dial.init();
+        dial.rotate("L50")?;
+
+        assert_eq!(dial.passed_zero, 1);
+        assert_eq!(dial.pointed_zero, 1);
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn multiple_rotations() -> Result<(), Box<dyn Error>> {
+        let mut dial = Dial::new();
+        dial.rotate("R150")?;
+
+        assert_eq!(dial.passed_zero, 2);
+        assert_eq!(dial.pointed_zero, 1);
+
+        dial.rotate("L240")?;
+
+        assert_eq!(dial.passed_zero, 4);
+        assert_eq!(dial.pointed_zero, 1);
+
+        dial.rotate("R40")?;
+
+        assert_eq!(dial.passed_zero, 5);
+        assert_eq!(dial.pointed_zero, 2);
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn rotation_from_zero() -> Result<(), Box<dyn Error>> {
+        let mut dial = Dial::new();
+        dial.position = 0;
+
+        dial.rotate("R150")?;
+
+        assert_eq!(dial.passed_zero, 1);
+        assert_eq!(dial.pointed_zero, 0);
+
+        dial.rotate("L250")?;
+
+        assert_eq!(dial.passed_zero, 4);
+        assert_eq!(dial.pointed_zero, 1);
+
+        dial.rotate("L200")?;
+
+        assert_eq!(dial.passed_zero, 6);
+        assert_eq!(dial.pointed_zero, 2);
+
+        Ok(())
+    }
 }
